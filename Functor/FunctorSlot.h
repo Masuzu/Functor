538aa85be33764b199e2a2a0c2fa7486d0109c38
Scende <anveena.menethil@gmail.com>
2014-11-14 00:06:17 +0100

    no message

---
+++ Functor/FunctorSlot.h
@@ -0,0 +1,299 @@
+#ifndef __FUNCTOR_SLOT_H__FunctorSlotBase
+#define __FUNCTOR_SLOT_H__
+
+#include <memory>
+
+template<typename R>
+class FunctorSlotBase
+{
+public:
+	virtual ~FunctorSlotBase() {}
+	virtual R operator()(void *, void *, void *)	const = 0;
+	virtual R operator()(void *, void *)	const = 0;
+	virtual R operator()(void *)	const = 0;
+	virtual R operator()()	const = 0;
+};
+
+template<typename R, typename T, typename U>
+class MemberFunctionSlot1 : public FunctorSlotBase<R>
+{
+public:
+	//! Member function slot type.
+	typedef R(T::*MemberFunctionType)(U);
+
+	MemberFunctionSlot1(MemberFunctionType func, T* obj, U arg) :
+		d_function(func),
+		d_object(obj),
+		d_arg(arg)
+	{}
+
+	MemberFunctionSlot1(MemberFunctionType func, T* obj) :
+		d_function(func),
+		d_object(obj)
+	{}
+
+	virtual R operator()()	const
+	{
+		return (d_object->*d_function)(d_arg);
+	}
+
+	virtual R operator()(void *arg)	const
+	{
+		return (d_object->*d_function)(reinterpret_cast<U>(arg));
+	}
+
+private:
+	MemberFunctionType d_function;
+	T* d_object;
+	U d_arg;
+
+	virtual R operator()(void *arg1, void *arg2)	const
+	{
+		return (d_object->*d_function)(reinterpret_cast<U>(arg1));
+	}
+
+	virtual R operator()(void *arg1, void *arg2, void *arg3)	const
+	{
+		return (d_object->*d_function)(reinterpret_cast<U>(arg1));
+	}
+};
+
+template<typename R, typename T, typename U, typename V>
+class MemberFunctionSlot2 : public FunctorSlotBase<R>
+{
+public:
+	//! Member function slot type.
+	typedef R(T::*MemberFunctionType)(U, V);
+
+	MemberFunctionSlot2(MemberFunctionType func, T* obj, U arg1, V arg2) :
+		d_function(func),
+		d_object(obj),
+		d_arg1(arg1),
+		d_arg2(arg2)
+	{}
+
+	MemberFunctionSlot2(MemberFunctionType func, T* obj, U arg1) :
+		d_function(func),
+		d_object(obj),
+		d_arg1(arg1)
+	{}
+
+	MemberFunctionSlot2(MemberFunctionType func, T* obj) :
+		d_function(func),
+		d_object(obj)
+	{}
+
+	virtual R operator()()	const
+	{
+		return (d_object->*d_function)(d_arg1, d_arg2);
+	}
+
+	virtual R operator()(void *arg)	const
+	{
+		return (d_object->*d_function)(reinterpret_cast<U>(arg), d_arg2);
+	}
+
+	virtual R operator()(void *arg1, void *arg2)	const
+	{
+		return (d_object->*d_function)(reinterpret_cast<U>(arg1), reinterpret_cast<V>(arg2));
+	}
+
+private:
+	MemberFunctionType d_function;
+	T* d_object;
+	U d_arg1;
+	V d_arg2;
+
+	virtual R operator()(void *arg1, void *arg2, void *arg3)	const
+	{
+		return (d_object->*d_function)(reinterpret_cast<U>(arg1), reinterpret_cast<V>(arg2));
+	}
+};
+
+template<typename R, typename T, typename U, typename V, typename W>
+class MemberFunctionSlot3 : public FunctorSlotBase<R>
+{
+public:
+	//! Member function slot type.
+	typedef R(T::*MemberFunctionType)(U, V, W);
+
+	MemberFunctionSlot3(MemberFunctionType func, T* obj, U arg1, V arg2, W arg3) :
+		d_function(func),
+		d_object(obj),
+		d_arg1(arg1),
+		d_arg2(arg2),
+		d_arg3(arg3)
+	{}
+
+	MemberFunctionSlot3(MemberFunctionType func, T* obj, U arg1, V arg2) :
+		d_function(func),
+		d_object(obj),
+		d_arg1(arg1),
+		d_arg2(arg2)
+	{}
+
+	MemberFunctionSlot3(MemberFunctionType func, T* obj, U arg1) :
+		d_function(func),
+		d_object(obj),
+		d_arg1(arg1)
+	{}
+
+	MemberFunctionSlot3(MemberFunctionType func, T* obj) :
+		d_function(func),
+		d_object(obj)
+	{}
+
+	virtual R operator()()	const
+	{
+		return (d_object->*d_function)(d_arg1, d_arg2, d_arg3);
+	}
+
+	virtual R operator()(void *arg)	const
+	{
+		return (d_object->*d_function)(reinterpret_cast<U>(arg), d_arg2, d_arg3);
+	}
+
+	virtual R operator()(void *arg1, void *arg2)	const
+	{
+		return (d_object->*d_function)(reinterpret_cast<U>(arg1), reinterpret_cast<V>(arg2), d_arg3);
+	}
+
+	virtual R operator()(void *arg1, void *arg2, void *arg3)	const
+	{
+		return (d_object->*d_function)(reinterpret_cast<U>(arg1), reinterpret_cast<V>(arg2), reinterpret_cast<W>(arg3));
+	}
+
+private:
+	MemberFunctionType d_function;
+	T* d_object;
+	U d_arg1;
+	V d_arg2;
+	W d_arg3;
+};
+
+template<typename R, typename T, typename U>
+class FunctionPointerSlot1 : public FunctorSlotBase<R>
+{
+public:
+	FunctionPointerSlot1(T* function, U arg) :
+		d_function(function),
+		d_arg(arg)
+	{}
+
+	virtual R operator()()	const
+	{
+		return (*d_function)(d_arg);
+	}
+
+	virtual R operator()(void *arg)	const
+	{
+		return (*d_function)(reinterpret_cast<U>(arg));
+	}
+
+private:
+	T* d_function;
+	U d_arg;
+};
+
+template<typename R>
+class Functor
+{
+public:
+	Functor()
+	{
+	}
+
+	Functor(const Functor &foo)
+	{
+		d_functor_impl = foo.d_functor_impl;
+	}
+
+	R operator()()	const
+	{
+		return (*d_functor_impl)();
+	}
+
+	template<typename U>
+	R operator()(U arg)	const
+	{
+		return (*d_functor_impl)(arg);
+	}
+
+	template<typename U, typename V>
+	R operator()(U arg1, V arg2)	const
+	{
+		return (*d_functor_impl)(arg1, arg2);
+	}
+
+	template<typename U, typename V, typename W>
+	R operator()(U arg1, V arg2, W arg3)	const
+	{
+		return (*d_functor_impl)(arg1, arg2, arg3);
+	}
+
+	template<typename R, typename T, typename U, typename V>
+	Functor(R(T::*function)(U, V), T* obj, U arg1, V arg2) :
+		d_functor_impl(std::shared_ptr<FunctorSlotBase<R>>(new MemberFunctionSlot2<R, T, U, V>(function, obj, arg1, arg2)))
+	{}
+
+	template<typename R, typename T, typename U, typename V>
+	Functor(R(T::*function)(U, V), T* obj, U arg) :
+		d_functor_impl(std::shared_ptr<FunctorSlotBase<R>>(new MemberFunctionSlot2<R, T, U, V>(function, obj, arg)))
+	{}
+
+	template<typename R, typename T, typename U, typename V>
+	Functor(R(T::*function)(U, V), T* obj) :
+		d_functor_impl(std::shared_ptr<FunctorSlotBase<R>>(new MemberFunctionSlot2<R, T, U, V>(function, obj)))
+	{}
+
+	template<typename R, typename T, typename U, typename V, typename W>
+	Functor(R(T::*function)(U, V, W), T* obj, U arg1, V arg2, W arg3) :
+		d_functor_impl(std::shared_ptr<FunctorSlotBase<R>>(new MemberFunctionSlot3<R, T, U, V, W>(function, obj, arg1, arg2, arg3)))
+	{}
+
+	template<typename R, typename T, typename U, typename V, typename W>
+	Functor(R(T::*function)(U, V, W), T* obj, U arg1, V arg2) :
+		d_functor_impl(std::shared_ptr<FunctorSlotBase<R>>(new MemberFunctionSlot3<R, T, U, V, W>(function, obj, arg1, arg2)))
+	{}
+
+	template<typename R, typename T, typename U, typename V, typename W>
+	Functor(R(T::*function)(U, V, W), T* obj, U arg1) :
+		d_functor_impl(std::shared_ptr<FunctorSlotBase<R>>(new MemberFunctionSlot3<R, T, U, V, W>(function, obj, arg1)))
+	{}
+
+	template<typename R, typename T, typename U, typename V, typename W>
+	Functor(R(T::*function)(U, V, W), T* obj) :
+		d_functor_impl(std::shared_ptr<FunctorSlotBase<R>>(new MemberFunctionSlot3<R, T, U, V, W>(function, obj)))
+	{}
+
+	template<typename R, typename T, typename U>
+	Functor(R(T::*function)(U), T* obj, U arg) :
+		d_functor_impl(std::shared_ptr<FunctorSlotBase<R>>(new MemberFunctionSlot1<R, T, U>(function, obj, arg)))
+	{}
+
+	template<typename R, typename T, typename U>
+	Functor(R(T::*function)(U), T* obj) :
+		d_functor_impl(std::shared_ptr<FunctorSlotBase<R>>(new MemberFunctionSlot1<R, T, U>(function, obj)))
+	{}
+
+	template<typename R, typename T, typename U>
+	Functor(T* function, U arg) :
+		d_functor_impl(std::shared_ptr<FunctorSlotBase<R>>(new FunctionPointerSlot1<R, T, U>(function, arg)))
+	{}
+
+	template<typename R, typename T, typename U>
+	Functor(T* function) :
+		d_functor_impl(std::shared_ptr<FunctorSlotBase<R>>(new FunctionPointerSlot1<R, T, U>(function)))
+	{}
+
+	void __stdcall Reset()
+	{
+		d_functor_impl.reset();
+	}
+
+private:
+	//! Points to the internal functor object to which we are bound.
+	std::shared_ptr<FunctorSlotBase<R>> d_functor_impl;
+};
+
+#endif
--- Functor/Main.cpp
+++ Functor/Main.cpp
@@ -1,4 +1,5 @@
 #include "Functor.h"
+#include "FunctorSlot.h"
 
 #include <functional>
 
@@ -6,6 +7,11 @@ class A
 {
 public:
 	int foo(double, char)
+	{
+		return 0;
+	}
+
+	int foo2(double *, char *)
 	{
 		return 0;
 	}
@@ -20,4 +26,7 @@ int main()
 	// Now do the same thing with std::function
 	std::function<int(double, char)> test2 = std::bind(&A::foo, &a, std::placeholders::_1, std::placeholders::_2);
 	test2(3.14, 'c');
+
+	Functor<int> test3(&A::foo2, &a);
+	test3(nullptr, nullptr);	// Will not work with non pointer arguments
 }
